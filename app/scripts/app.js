b1dedf7333c0e8d70dd2a3a96f4e15e41265e66a
huudatHust <datk571102@gmail.com>
2016-02-26 01:29:34 +0700

    commit 25-1-2016 dashboard

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ angular
   ])
   .config(function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/about');
     
     $stateProvider
         
