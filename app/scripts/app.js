ad243a8b395b001d89a6d977ce5d6e73e32cb33e
huudatHust <datk571102@gmail.com>
2016-02-26 23:25:38 +0700

    complete dashboard

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@
  * Main module of the application.
  */
 
-angular
+var app =  angular
   .module('generatorYeomanApp', [
     'ngAnimate',
     'ngCookies',
@@ -18,22 +18,90 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
+  .run(
+    [          '$rootScope', '$state', '$stateParams',
+      function ($rootScope,   $state,   $stateParams) {
+          $rootScope.$state = $state;
+          $rootScope.$stateParams = $stateParams;        
+      }
+    ]
+  )
   .config(function($stateProvider, $urlRouterProvider) {
     
-    $urlRouterProvider.otherwise('/about');
+    $urlRouterProvider.otherwise('/home');
     
     $stateProvider
         
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
-            templateUrl: 'views/main.html'
+            templateUrl: 'views/main.html',
+             controller : 'MainCtrl',
         })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
               url : '/about',
               templateUrl : 'views/about.html'
-        });
+        })
+
+        //about template =====================================================
+        .state('app', {
+                  abstract: true,
+                  url: '/app',
+                  templateUrl: 'views/tpl/app.html'
+        })
+       .state('app.dashboard', {
+                url: '/dashboard',
+                templateUrl: 'views/tpl/dashboard.html',
+        })
+       .state('app.blank', {
+            url : '/blank',
+            templateUrl : 'views/tpl/blank.html'
+       })
+       .state('app.buttons', {
+            url : '/buttons',
+            templateUrl : 'views/tpl/buttons.html'
+       })
+       .state('app.flot', {
+            url : '/flot',
+            templateUrl : 'views/tpl/flot.html'
+       })
+       .state('app.forms', {
+            url : '/forms',
+            templateUrl : 'views/tpl/forms.html'
+       })
+       .state('app.grid', {
+            url : '/grid',
+            templateUrl : 'views/tpl/grid.html'
+       })
+       .state('app.icons', {
+            url : '/icons',
+            templateUrl : 'views/tpl/icons.html'
+       })
+       .state('app.morris', {
+            url : '/morris',
+            templateUrl : 'views/tpl/morris.html'
+       })
+       .state('app.notifications', {
+            url : '/notifications',
+            templateUrl : 'views/tpl/notifications.html'
+       })
+       .state('app.panel', {
+            url : '/panel',
+            templateUrl : 'views/tpl/panels-wells.html'
+       })
+       .state('app.table', {
+            url : '/table',
+            templateUrl : 'views/tpl/tables.html'
+       })
+       .state('app.typography', {
+            url : '/typography',
+            templateUrl : 'views/tpl/typography.html'
+       })
+       .state('loginTpl', {
+          url : '/app/login',
+          templateUrl : '/views/tpl/login.html'
+       });
         
 });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,9 @@
  * # MainCtrl
  * Controller of the generatorYeomanApp
  */
-angular.module('generatorYeomanApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+ app.controller('MainCtrl', function ($scope) {
+    $scope.welcome = "Welcome to admin itbee !";
+    $scope.call = function(){
+    	alert("hello");
+    }
   });
